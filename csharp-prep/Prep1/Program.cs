81c771aa9190fbda658bb4845567885219dabd29
seangariety <gar24092@byui.edu>
2026-09-21 09:14:50 -0600

    "Your name's..."

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -11,6 +11,6 @@ class Program
         Console.Write("Please enter your last name: ");
         lastName = Console.ReadLine();
 
-        Console.WriteLine($"The name's {lastName}. {firstName} {lastName}. ");
+        Console.WriteLine($"Your name's {lastName}. {firstName} {lastName}. ");
     }
 }
