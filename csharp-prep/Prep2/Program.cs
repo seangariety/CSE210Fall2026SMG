178a0d907daae6e42283e9df3866902997a910a3
seangariety <gar24092@byui.edu>
2026-09-21 20:05:56 -0600

    if else grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,8 +8,23 @@ class Program
         Console.WriteLine("What is your percentage grade? ");
         string userinput = Console.ReadLine();
         int number = int.Parse(userinput);
+        string grade;
+        
 
-        Console.WriteLine($"Your grade is {number}");
+        if (number >= 90) grade = "A";
+        else if (number >= 80) grade = "B";
+        else if (number >= 70) grade = "C";
+        else if (number >= 60) grade = "D";
+        else if (number < 60) grade = "F";
+        else
+        {
+            Console.WriteLine("How did we even get here?");
+            grade = "...Umm... something broke.";
+        }
+
+
+
+        Console.WriteLine($"Your letter grade is {grade}");
         
     }
 }
