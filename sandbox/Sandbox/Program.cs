8d24be403f414c0ab06a458bcc01f577fce18447
seangariety <gar24092@byui.edu>
2026-09-23 10:15:50 -0600

    Sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,7 +3,48 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("What's up ganglers? Welcome to the slimefest!");
-    }
+        {
+            Console.WriteLine("What's up ganglers? Welcome to the slimefest!");
+        }
+
+        
+    
+        {
+            bool done = false;
+
+            while (!done)
+            {
+                Console.Write("Are we done (y/n): ");
+                done = Console.ReadLine().ToLower() == "y";
+            }
+        }
+    
+
+
+    
+        // {
+        //     bool done;
+
+        //     do
+        //     {
+        //         Console.Write("Are we done (y/n): ");
+        //         done = Console.ReadLine.ToLower == "y";
+        //     } while (!done);
+        // }
+    
+
+
+    
+        // for (int i = 0; i > 10; i++)
+        // {
+        //     Console.WriteLine(i);
+        // }
+    
+
 }
+}
+
+
+
